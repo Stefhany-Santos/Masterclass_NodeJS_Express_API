c8e648318d1bfcc14412b3979d5bf46cdeb0220e
Stefhany Santos <stefhany.contato@outlook.com>
2024-04-28 17:16:30 -0300

    🚀 feat: Criação e configuração do arquivo de rotas
    • Cria o arquivo de rotas e implementa algumas configurações iniciais
    • Importa o arquivo de rotas no arquivo app.js e configura para que sempre seja utilizado para rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const router = require('./router');
 
 const app  = express();
 
 app.get('/tasks', (request, response) => response.status(200).send('Hello world!'));
-
+app.use(router);
 
 module.exports = app;
